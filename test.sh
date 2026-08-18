108c531a6bcfe7f3248bc94a6bd1bfdd1494e911
CyberMask11 <johndray977@gmail.com>
2026-08-18 14:51:52 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-for i in {1..20}; do
-    curl  \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d '{"username":"daemon","password":"rogueprinc"}' \
-    http://localhost:8080/login
-done
